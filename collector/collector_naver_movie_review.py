8a172ed680dbc3d36d50909dba3afaf93d58c1ae
son0210 <105830624+son0210@users.noreply.github.com>
2022-11-10 01:24:39 +0900

    finished review collect

---
+++ collector/collector_naver_movie_review.py
@@ -0,0 +1,67 @@
+import re
+import math
+import requests
+from bs4 import BeautifulSoup
+
+
+# 함수 생성 > 아무 동작 X
+# 반드시 생성 후 호출을 통해서 사용
+# 제목 수집 -> movie_code: 네이버 영화 코드(6자리 코드)
+def movie_title_crawler(movie_code):
+    url = f'https://movie.naver.com/movie/bi/mi/point.naver?code={movie_code}'
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    title = doc.select('h3.h_movie > a')[0].get_text()
+    return title
+
+
+# 리뷰 수집(리뷰, 평점, 작성자, 작성일) + 제목
+def movie_review_crawler(movie_code):
+    title = movie_title_crawler(movie_code)  # 제목 수집
+    print(f'>> Start collecting movies for: {title}')
+
+    # set{제목, 리뷰, 평점, 작성자, 작성일}
+    url = f'https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code={movie_code}&type=after&onlyActualPointYn=N&onlySpoilerPointYn=N&order=sympathyScore&page=1'
+    result = requests.get(url)
+    doc = BeautifulSoup(result.text, 'html.parser')
+    all_count = doc.select('strong.total > em')[0].get_text()  # 리뷰 개수
+    # print(type(all_count))
+    # ,포함된 숫자 > 형변환 불가
+    # 1. 숫자만 추출 : 정규식
+    numbers = re.sub(r'[^0-9]', '', all_count)
+    pages = math.ceil(int(numbers)/10)
+    print(f'The total number of pages to collect is {pages}')
+
+    # 해당 페이지 리뷰 수집
+    count = 0  # 전체 리뷰 수를 count
+    for page in range(1, pages+1):
+        url = f'https://movie.naver.com/movie/bi/mi/pointWriteFormList.naver?code={movie_code}&type=after&onlyActualPointYn=N&onlySpoilerPointYn=N&order=sympathyScore&page={page}'
+        result = requests.get(url)
+        doc = BeautifulSoup(result.text, 'html.parser')
+        review_list = doc.select('div.score_result > ul > li')  # 1page의 리뷰 10건
+
+        for i, one in enumerate(review_list):  # review 1건씩 수집
+            review = one.select('div.score_reple > p > span')[-1].get_text().strip()  # 리뷰 내용
+            score = one.select('div.star_score > em')[0].get_text()  # 평점
+
+            # 날짜 시간 → 날짜만 추출
+            # - 예 : 2022.10.19 15:28 → 2022.10.19
+            # - 날짜는 항상 16글자로 구성
+            original_date = one.select('div.score_reple dt > em')[1].get_text()
+
+            # 문자열 추출
+            # [시작:끝+1], 끝은 포함 X
+            # [:15] 0~14
+            # [3:] 3~끝까지
+            date = original_date[:10]
+
+            original_writer = one.select('div.score_reple dt > em')[0].get_text().strip()
+            idx_end = original_writer.find('(')  # ( 의 인덱스 번호
+            writer = original_writer[:idx_end]
+
+            count += 1
+            print(f"## 리뷰 → {count} #######################################################################")
+            print(f'# Review: {review}')
+            print(f'# Writer: {writer}')
+            print(f'# Score: {score}')
+            print(f'# Date: {date}')
